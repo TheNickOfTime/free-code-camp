d9509df30b72267b0b6d2c1ed72c98eeba2c5ea9
Nick Cunningham <thenickoftime.x@gmail.com>
2024-02-13 15:54:04 -0800

    Adding timestamp microservice project

---
+++ 06 - Backend Development and APIs/01 - Timestamp Microservice/index.js
@@ -0,0 +1,58 @@
+// index.js
+// where your node app starts
+
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api", (req, res) => {
+  const date = new Date(Date.now());
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
+});
+
+app.get("/api/:date?", (req, res) => {
+  const stringDate = new Date(req.params.date);
+  const epochDate = new Date(parseInt(req.params.date))
+  const date = stringDate != "Invalid Date" ? stringDate : epochDate;
+  // console.log(date);
+
+  if (date != "Invalid Date") {
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString(),
+    });
+  } else {
+    res.json({
+      error: "Invalid Date"
+    })
+  }
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
